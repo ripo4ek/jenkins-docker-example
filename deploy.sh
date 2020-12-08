136ddacac9df23ee9cd0185904919f469f34ad0f
ripo4ek <Lyap-andr@yandex.ru>
2020-12-08 22:45:49 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-docker kill mobycounter redis > /dev/null 2>&1
-docker rm mobycounter redis > /dev/null 2>&1
-docker-compose up -d
+docker network create -d bridge app-network
+docker run  --network=app-network --name redis -d redis:3.0 
+docker run  --network=app-network --name dnmonster -d amouat/dnmonster:1.0
+docker run  --network=app-network -p 6565:9090 --name identidock -d ripo4ek/flask-redis-avatar:latest
